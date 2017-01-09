90f4274bcdb869c590d3d66d692e499fbf51b657
Pete Whitby <petejw@theswamp.co.uk>
2017-01-09 16:05:35 +0000

    initial commit

---
+++ IdentityServer/Scopes.cs
@@ -0,0 +1,28 @@
+﻿using IdentityServer3.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+	static class Scopes
+	{
+		/*
+			Registering the API
+			APIs are modeled as scopes - you need to register all APIs that you want to be able to request access tokens for. 
+			For that we create a class that returns a list of Scope:
+		*/
+		public static List<Scope> Get()
+		{
+			return new List<Scope>()
+			{
+				new Scope()
+				{
+					Name = "api1"
+				}
+			};
+		}
+	}
+}
